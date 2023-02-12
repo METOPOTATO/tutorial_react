f948061da2bb6586d68955106367b478ba8ff144
LinhBQ <linhstartwork@gmail.com>
2023-02-12 11:13:58 +0700

    update code

--- src/BLogDetail.js
+++ src/BLogDetail.js
@@ -2,14 +2,18 @@ import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 const BLogDetail = () => {
   const { id } = useParams();
-  const { data: blog, isPending, error} = useFetch('http://127.0.0.1:5000/blogs/{id}' + id);
+  const { data: blog, isPending, error} = useFetch('http://127.0.0.1:5000/blogs/' + id);
 
   return ( 
     <div className="blog-details">
       { isPending && <div>Loading...</div>}
       { error && <div>{ error }</div>}
       { blog && (
-        <article></article>
+        <article>
+          <h2>{ blog.title }</h2>
+          <p>Written by {  blog.author }</p>
+          <p>{ blog.body }</p>
+        </article>
       )}
     </div>
    );
